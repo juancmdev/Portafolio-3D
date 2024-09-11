9d41ea036c9ef97bb7c2e185f161b7118ae4def8
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2024-09-11 16:45:42 -0500

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React, { Suspense } from "react"
+import { Suspense } from "react"
 import { Canvas } from "@react-three/fiber"
+import Loader from '../components/Loader'
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   POPUP
@@ -10,12 +11,12 @@ const Home = () => {
   
   return (
   <section className="w-full h-screen relative">
-    <Canvas className="w-full h-screen bg-transparent" camera={{near: 0.1, far: 1000}}>
+    {/* <Canvas className="w-full h-screen bg-transparent" camera={{near: 0.1, far: 1000}}>
 
       <Suspense fallback={<Loader />}>
 
       </Suspense>
-    </Canvas>
+    </Canvas> */}
   </section>
   )
 }
