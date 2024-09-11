925182edc54fcd7f29d37dd1619a658c5992a4e7
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2024-09-11 17:11:58 -0500

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
+import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react"
-import { Canvas } from "@react-three/fiber"
 import Loader from '../components/Loader'
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -8,15 +8,17 @@ import Loader from '../components/Loader'
 
 const Home = () => {
   
-  
   return (
   <section className="w-full h-screen relative">
-    {/* <Canvas className="w-full h-screen bg-transparent" camera={{near: 0.1, far: 1000}}>
+    <Canvas 
+      className="w-full h-screen bg-transparent" 
+      camera={{near: 0.1, far: 1000}}
+    >
 
       <Suspense fallback={<Loader />}>
 
       </Suspense>
-    </Canvas> */}
+    </Canvas>
   </section>
   )
 }
