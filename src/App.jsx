e19851e2c1f2368fe561bd3e79d2fb4c13bdb548
juancmdev <juancmorenoj@gmail.com>
2024-09-14 21:03:14 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import { Home, About, Projects, Contact } from "./pages";
 
 const App = () => {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
+  POPUP
+</div> */
+}
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      ></Canvas>
+    </section>
+  );
 };
 
 export default Home;
