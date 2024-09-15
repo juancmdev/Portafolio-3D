ab8c2c72c83930f6891d8100dbab07997ddd56c7
juancmdev <juancmorenoj@gmail.com>
2024-09-14 21:36:27 -0500

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
@@ -14,6 +15,13 @@ const Home = () => {
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       ></Canvas>
+      <Suspense fallback={<Loader />}>
+        <directionalLight />
+        <ambientLight />
+        <pointLight />
+        <spotLight />
+        <hemisphereLight />
+      </Suspense>
     </section>
   );
 };
