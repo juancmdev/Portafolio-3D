dd56d619dd3fd0543e287e937be47dac59d2cb67
juancmdev <juancmorenoj@gmail.com>
2024-09-14 19:28:12 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -7,10 +11,10 @@ const App = () => {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={"Home"} />
-          <Route path="/about" element={"About"} />
-          <Route path="/projects" element={"Projects"} />
-          <Route path="/contact" element={"Contact"} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
