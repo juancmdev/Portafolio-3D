400eda44c5178b261eba255fa9d723a229bdb1e8
juancmdev <juancmorenoj@gmail.com>
2024-09-14 19:21:14 -0500

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,26 @@
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white flex justify-center items-center font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">AH</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
